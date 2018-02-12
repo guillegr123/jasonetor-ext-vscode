93c917fb1875e83e8a335814c70316251d43b0f9
Guillermo Gutierrez <guillegr123@gmail.com>
2018-02-11 23:07:05 -0600

    Added test command.

--- src/extension.ts
+++ src/extension.ts
@@ -14,14 +14,19 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposableSayHello = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
     });
 
-    context.subscriptions.push(disposable);
+    let disposableSideEditor = vscode.commands.registerCommand('extension.sideEditor', () => {
+        vscode.window.showInformationMessage('TODO: Show side editor');
+    });
+
+    context.subscriptions.push(disposableSayHello);
+    context.subscriptions.push(disposableSideEditor);
 }
 
 // this method is called when your extension is deactivated
