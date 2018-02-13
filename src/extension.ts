d280bd7ea539886e2045ee7cb9f106ae9e75b04b
Guillermo Gutierrez <guillegr123@gmail.com>
2018-02-12 22:41:53 -0600

    Display HTML preview panel with JSON document content.

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,79 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "jasonetor" is now active!');
+	let previewUri = vscode.Uri.parse('css-preview://authority/css-preview');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposableSayHello = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+	class TextDocumentContentProvider implements vscode.TextDocumentContentProvider {
+		private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+		public provideTextDocumentContent(uri: vscode.Uri): string {
+			return this.createCssSnippet();
+		}
 
-    let disposableSideEditor = vscode.commands.registerCommand('extension.sideEditor', () => {
-        vscode.window.showInformationMessage('TODO: Show side editor');
-    });
+		get onDidChange(): vscode.Event<vscode.Uri> {
+			return this._onDidChange.event;
+		}
 
-    context.subscriptions.push(disposableSayHello);
-    context.subscriptions.push(disposableSideEditor);
-}
+		public update(uri: vscode.Uri) {
+			this._onDidChange.fire(uri);
+		}
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+		private createCssSnippet() {
+			let editor = vscode.window.activeTextEditor;
+			if (!(editor.document.languageId === 'json')) {
+				return this.snippet("Active editor doesn't show a JSON document - no JSON to edit.")
+			}
+			return this.extractSnippet();
+		}
+
+		private extractSnippet(): string {
+			let editor = vscode.window.activeTextEditor;
+			let text = editor.document.getText();
+			let selStart = editor.document.offsetAt(editor.selection.anchor);
+			let propStart = text.lastIndexOf('{', selStart);
+			let propEnd = text.indexOf('}', selStart);
+
+			if (propStart === -1 || propEnd === -1) {
+				return this.snippet("Cannot determine the rule's properties.");
+			} else {
+				return this.snippet(text);
+			}
+		}
+
+		private snippet(text: string): string {
+			return `
+                <body>
+                    <pre>
+                        ${text}
+                    </pre>
+				</body>`;
+		}
+	}
+
+	let provider = new TextDocumentContentProvider();
+	let registration = vscode.workspace.registerTextDocumentContentProvider('css-preview', provider);
+
+	vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => {
+		if (e.document === vscode.window.activeTextEditor.document) {
+			provider.update(previewUri);
+		}
+	});
+
+	vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
+		if (e.textEditor === vscode.window.activeTextEditor) {
+			provider.update(previewUri);
+		}
+	})
+
+	let disposable = vscode.commands.registerCommand('extension.jsonSideEditor', () => {
+		return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'JSON Editor').then((success) => {
+		}, (reason) => {
+			vscode.window.showErrorMessage(reason);
+		});
+	});
+
+	context.subscriptions.push(disposable, registration);
 }
